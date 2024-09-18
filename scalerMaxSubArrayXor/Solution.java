506c1a5291bd9987c6996736ca968bb83eb3e5d7
Kuaminika <55932084+kuaminika@users.noreply.github.com>
2024-09-17 22:17:29 -0400

    added comment

--- scalerMaxSubArrayXor/Solution.java
+++ scalerMaxSubArrayXor/Solution.java
@@ -1,6 +1,19 @@
 package scalerMaxSubArrayXor;
 
 import java.util.ArrayList;
+/**
+ * Given an array, A of integers of size N. Find the subarray AL, AL+1, AL+2, ... AR with 1<=L<=R<=N, which has maximum XOR value.
+
+    NOTE: If there are multiple subarrays with the same maximum value, return the subarray with minimum length. If the length is the same, return the subarray with the minimum starting index.
+
+
+ * 
+ * 
+ * 
+ * 
+ */
+
+
 
 public class Solution {
     public ArrayList<Integer> solve(ArrayList<Integer> A) {
