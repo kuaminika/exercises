8746462a0e3d928f22f6d936fe9b4427549b9ce3
Kuaminika <55932084+kuaminika@users.noreply.github.com>
2024-09-18 23:51:44 -0400

    removed comments for RecoverBinarySearchTree

--- RecoverBinarySearchTree/RecoverBinarySearchTree/Solution.java
+++ RecoverBinarySearchTree/RecoverBinarySearchTree/Solution.java
@@ -13,8 +13,9 @@ public class Solution {
         ArrayList<Integer> result = new ArrayList<Integer>();
 
       
-        result.add(tracker.inv1);
-        result.add(tracker.inv2);
+       
+        result.add(Math.min(tracker.inv1,tracker.inv2));
+        result.add(Math.max(tracker.inv1,tracker.inv2));
         return result;
     }
 
@@ -23,22 +24,16 @@ public class Solution {
  
     public void doInOrder(TreeNode A, InversionTrackingStuff tracker,String indent)
     {
-        //logLn(indent+"prev:"+prev+",inv1:"+inv1+",inv2:"+inv2);
+   
       
         if(A == null) return;
 
         
-        logLn(indent+"doing node:"+A.val+" [prev:"+tracker.prev+",inv1:"+tracker.inv1+",inv2:"+tracker.inv2+"]");
-
-        logLn(indent+"will do left");
+  
         doInOrder(A.left,tracker,indent+" ");
-        logLn(indent+"finish do left");
-
-        logLn(indent+"did left node of ("+A.val+") [prev:"+tracker.prev+",inv1:"+tracker.inv1+",inv2:"+tracker.inv2+"]");
         
         boolean inversionOccurred = tracker.prev != 0 && (tracker.prev> A.val);
 
-        logLn(indent+ "inversion occured:"+inversionOccurred);
         if(inversionOccurred)
         {
             if(tracker.inv1 == 0 )
@@ -49,29 +44,18 @@ public class Solution {
             }
             else
             {
-               // tracker.inv1 = tracker.inv2;
                 tracker.inv2 = A.val;
             }
         }
 
         tracker.prev = A.val;
-        logLn(indent+"will do right");
         doInOrder(A.right, tracker , indent+" ");
-        logLn(indent+"finish do right");
+
     }
 
 
 
-    public   void logLn(String s)
-    {
 
-       System.out.println(s);
-   }
-   private   void log(String s)
-   {
-
-       System.out.print(s);
-   }
 }
 
 class InversionTrackingStuff
