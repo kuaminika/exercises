da2db78f68f8bb34e6f149972cb6dc34ec69690c
Kuaminika <55932084+kuaminika@users.noreply.github.com>
2025-01-16 22:00:58 -0500

    updated template to key in 2D matrix.

--- template/Driver.java
+++ template/Driver.java
@@ -21,6 +21,27 @@ public class Driver{
     }
 
     
+    public static ArrayList<ArrayList<Integer>> inputStrForMatrix(String input)
+    {
+      input = input.substring(2, input.length() - 2); // Remove the outer "[[ ... ]]"
+      ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+
+      for (String iterable_element :   input.split("\\],\\[")) {
+        
+        if(iterable_element.length()==0)
+        {
+        // result.add(new ArrayList<Integer>());
+          continue;
+
+        }
+        result.add(  inputInt(iterable_element));
+      }
+
+      return result;
+    }
+
+
+
     public static ArrayList<String> inputStr(String input)
     {
         ArrayList<String> integers = new ArrayList<>();
